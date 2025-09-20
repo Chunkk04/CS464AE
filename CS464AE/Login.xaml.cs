8e9ba92ed6731371b125a53c7a28d67bb44cde8e
AnhKyDepTrai <kyn46686@gmail.com>
2025-09-20 21:25:04 +0700

    Khởi tạo dự án

---
+++ CS464AE/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS464AE
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+  
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+     
+    }
+}
---
+++ CS464AE/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS464AE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddTodo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditTodo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteTodo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAllTodos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPendingTodos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCompletedTodos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnOverdueTodos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStatistics_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTestDB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDiagnostic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LvTodos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
